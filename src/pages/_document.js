9875db409fa647dc79216863ffe2ef69a477b59d
alperenjs <alperensozen96@gmail.com>
2023-04-06 17:29:22 +0300

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -6,8 +6,11 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+        <div id="myportal">
+          <iframe src="http://www.alperensozen.com"></iframe>
+        </div>
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
